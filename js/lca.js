43913a67ee1043c1922f5b43209503194f60f0f6
georgemakrakis <georgemakrakis88@gmail.com>
2020-10-25 12:59:23 -0600

    Some more functionality moved

--- js/lca.js
+++ js/lca.js
@@ -2,41 +2,41 @@
 var resultProcess1 = 0;
 var resultProcess2 = 0;
 
-function initializeChart(ctx){
+function initializeChart(ctx) {
   var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'bar',
 
     // The data for our dataset
     data: {
-        labels: ['Current Approach', 'Proposed Approach'],
-        datasets: []          
+      labels: ['Current Approach', 'Proposed Approach'],
+      datasets: []
     },
 
     // Configuration options go here
     options: {
-        scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true,
-                  //max: 3000,
-                  stepSize: 500000
-              }
-          }]
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            //max: 3000,
+            stepSize: 500000
+          }
+        }]
       }
-    }      
+    }
   });
   return chart;
 }
 
-function createResultsFromCSVTable(table){    
-      
+function createResultsFromCSVTable(table) {
+
   // if (this.value == 'socialMethod' && table != null) {
-  if (table != null) {                   
+  if (table != null) {
 
     // We only need from columns 1-6 correct? (No the first column?)
-    var columnData = table.columns([1,2,3,4,5,6]).data();
-    var rowsData = table.rows().data();          
+    var columnData = table.columns([1, 2, 3, 4, 5, 6]).data();
+    var rowsData = table.rows().data();
 
     var uniqueCountries = Array.from(new Set(columnData[1]));
 
@@ -45,39 +45,36 @@ function createResultsFromCSVTable(table){
 
     // We traverse all rows to perform the required multiplication
     rowsData.each(row => {
-      if($.isNumeric(row[6])){
+      if ($.isNumeric(row[6])) {
         w_i *= row[6]; // be carefull, here we count from 0..n not from 1
       }
-      else{        
+      else {
         w_i_ok = false; // even if we find one string in the w_i's we put zero to that value
         return false;
-      }                  
-    });                
+      }
+    });
 
     // If we have noticed a string we return
-    if(!w_i_ok)
-    {          
-      return{
+    if (!w_i_ok) {
+      return {
         error: "One or more values in w_i is not a number"
       };
     }
-    
+
     // The folloing value represent the equation: Indicator Value = (Sum of number of metrics * sum of number of locations * weight_i(Value_i))/number of metrics
     // be carefull, here whe count from 0..n not from 1
     var result = (columnData[0].length * uniqueCountries.length * w_i) / columnData[0].length;
 
     // Round result to 2 decimal points
     result = Math.round((result + Number.EPSILON) * 100) / 100;
-    
-    if(resultProcess1 != 0 && resultProcess2 == 0)
-    {
+
+    if (resultProcess1 != 0 && resultProcess2 == 0) {
       resultProcess2 = result;
     }
-    if(resultProcess1 == 0 && resultProcess2 == 0)
-    {
+    if (resultProcess1 == 0 && resultProcess2 == 0) {
       resultProcess1 = result;
     }
-              
+
     // console.log(result);    
 
     return {
@@ -88,7 +85,32 @@ function createResultsFromCSVTable(table){
   }
 }
 
-function updateChart(chart, label, resultProcess1, resultProcess2){
+function createResultsFromCustomTable(metrics, countries, w_i) {
+  var uniqueCountries = Array.from(new Set(countries));
+
+  var result = (metrics * uniqueCountries.length * w_i) / metrics;
+
+  // Round result to 2 decimal points
+  result = Math.round((result + Number.EPSILON) * 100) / 100
+
+  // console.log(result);
+
+  if (resultProcess1 != 0 && resultProcess2 == 0) {
+    resultProcess2 = result;
+  }
+  if (resultProcess1 == 0 && resultProcess2 == 0) {
+    resultProcess1 = result;
+  }
+
+  return {
+    result1: resultProcess1,
+    result2: resultProcess2,
+    error: null
+  };
+
+}
+
+function updateChart(chart, label, resultProcess1, resultProcess2) {
   var dataset = [
     {
       label: label,
@@ -99,4 +121,22 @@ function updateChart(chart, label, resultProcess1, resultProcess2){
   ];
   chart.data.datasets = dataset;
   chart.update();
+}
+
+function createPDF(canvasImg, list) {
+  //creates PDF from the img that the chart is converted to
+  var doc = new jspdf.jsPDF();
+  doc.setFontSize(11);
+
+  var text = []
+  for (var i in list) {
+    //console.log(list[i]);
+    text.push(list[i]);
+  }
+  doc.text(text, 10, 10);
+
+  // Add the chart/plot      
+  doc.addImage(canvasImg, 'JPEG', 20, 180, 160, 100, 'NONE');
+
+  doc.save('report.pdf');
 }
