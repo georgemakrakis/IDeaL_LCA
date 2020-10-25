adcb570b4521fd6858a2c78497862808a3b0dc12
georgemakrakis <georgemakrakis88@gmail.com>
2020-10-25 12:30:53 -0600

    Started moving functionality to separate file

--- js/lca.js
+++ js/lca.js
@@ -1,33 +1,102 @@
-function calculateTotal()
-{
-  let unit_price={
-    sugar: 22,
-    butter: 12,
-    eggs: 2,
-    vanilla:43    
-  };
-  let item_price={}
-  
-  item_price.sugar = ($("#qty_sugar").val() * unit_price.sugar )
-  $("#price_sugar").val(item_price.sugar);
-  
-  item_price.butter = ($("#qty_butter").val() * unit_price.butter )
-  $("#price_butter").val(item_price.butter);
-  
-  item_price.eggs = ($("#qty_eggs").val() * unit_price.eggs )
-  $("#price_eggs").val(item_price.eggs);  
-  
-  item_price.vanilla = ($("#qty_vanilla").val() * unit_price.vanilla )
-  $("#price_vanilla").val(item_price.vanilla);    
-  
-  let total = item_price.sugar + item_price.butter + item_price.eggs + item_price.vanilla;
+// Maybe we need this to be an array if we have multiple results for Life Excpectancy, Health etc.
+var resultProcess1 = 0;
+var resultProcess2 = 0;
 
- 
-  $("#total_value").text(total);
-  
+function initializeChart(ctx){
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['Current Approach', 'Proposed Approach'],
+        datasets: []          
+    },
+
+    // Configuration options go here
+    options: {
+        scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true,
+                  //max: 3000,
+                  stepSize: 500000
+              }
+          }]
+      }
+    }      
+  });
+  return chart;
 }
 
-$(function()
- {
-    $(".qty").on("change keyup",calculateTotal)
-})
+function createResultsFromCSVTable(table){    
+      
+  // if (this.value == 'socialMethod' && table != null) {
+  if (table != null) {                   
+
+    // We only need from columns 1-6 correct? (No the first column?)
+    var columnData = table.columns([1,2,3,4,5,6]).data();
+    var rowsData = table.rows().data();          
+
+    var uniqueCountries = Array.from(new Set(columnData[1]));
+
+    var w_i = 1;
+    var w_i_ok = true;
+
+    // We traverse all rows to perform the required multiplication
+    rowsData.each(row => {
+      if($.isNumeric(row[6])){
+        w_i *= row[6]; // be carefull, here we count from 0..n not from 1
+      }
+      else{        
+        w_i_ok = false; // even if we find one string in the w_i's we put zero to that value
+        return false;
+      }                  
+    });                
+
+    // If we have noticed a string we return
+    if(!w_i_ok)
+    {          
+      return{
+        error: "One or more values in w_i is not a number"
+      };
+    }
+    
+    // The folloing value represent the equation: Indicator Value = (Sum of number of metrics * sum of number of locations * weight_i(Value_i))/number of metrics
+    // be carefull, here whe count from 0..n not from 1
+    var result = (columnData[0].length * uniqueCountries.length * w_i) / columnData[0].length;
+
+    // Round result to 2 decimal points
+    result = Math.round((result + Number.EPSILON) * 100) / 100;
+    
+    if(resultProcess1 != 0 && resultProcess2 == 0)
+    {
+      resultProcess2 = result;
+    }
+    if(resultProcess1 == 0 && resultProcess2 == 0)
+    {
+      resultProcess1 = result;
+    }
+              
+    // console.log(result);    
+
+    return {
+      result1: resultProcess1,
+      result2: resultProcess2,
+      error: null
+    };
+  }
+}
+
+function updateChart(chart, label, resultProcess1, resultProcess2){
+  var dataset = [
+    {
+      label: label,
+      backgroundColor: 'rgb(0, 128, 255)',
+      borderColor: 'rgb(0, 128, 255)',
+      data: [resultProcess1, resultProcess2]
+    }
+  ];
+  chart.data.datasets = dataset;
+  chart.update();
+}
