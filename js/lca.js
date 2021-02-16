cb26f4315f23abadcbc02030845c966bc63f87a3
georgemakrakis <georgemakrakis88@gmail.com>
2021-02-16 12:00:57 -0700

    Changed equation image to base64, added to PDF

--- js/lca.js
+++ js/lca.js
@@ -112,7 +112,7 @@ function updateChart(chart, resultsLifeExpectancy, resultsEducation, resultsHeal
   chart.update();
 }
 
-function createPDF(canvasImg, LifeExpectancyTableImg, list) {
+function createPDF(canvasImg, LifeExpectancyTableImg, equation_image, list) {
   
   // console.log(LifeExpectancyTableImg);
 
@@ -142,18 +142,29 @@ function createPDF(canvasImg, LifeExpectancyTableImg, list) {
 
   lines.forEach(line => {
     if(line === "Name of the project, Name and Email:" || line === "Phase 1. Goal and scope definition:" 
-    || line === "Phase 2. Life cycle inventory (LCI):" || line === "Phase 3. Life cycle impact assessment (LCIA):"){
+    || line === "Phase 2. Life cycle inventory (LCI):" || line === "Phase 3. Life cycle impact assessment (LCIA):"
+    || line === "Results:"){
       var textWidth = doc.getTextWidth(line);
       doc.line(lMargin, y_margin+1, lMargin + textWidth, y_margin+1);
     }
     
     doc.text(lMargin,y_margin,line);
     y_margin+=10;
+
+    if(line === "Phase 3. Life cycle impact assessment (LCIA):"){
+      
+      if(equation_image!=null){
+        doc.addImage(equation_image, 'JPEG', lMargin, y_margin+1, 40, 22);
+        y_margin+=30;
+      } 
+    }
   });
   // doc.text(lMargin,20,lines);
 
   // Add the chart/plot      
   doc.addImage(canvasImg, 'JPEG', 20, 180, 160, 100, 'NONE');
+
+   
   // doc.addImage(LifeExpectancyTableImg, 'JPEG', 20, 100, 100, 80);
 
   doc.save('report.pdf');
